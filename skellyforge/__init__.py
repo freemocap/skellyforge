8aafaaf7cd566b74217f654c1aa55f8f0d31b400
jonmatthis <jonmatthis@gmail.com>
2024-07-18 10:19:49 -0400

    Bump version v2023.12.1005 -> v2024.07.1006

--- skellyforge/__init__.py
+++ skellyforge/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for basic_template_repo."""
 
 __package_name__ = "skellyforge"
-__version__ = "v2023.12.1005"
+__version__ = "v2024.07.1006"
 
 __author__ = """Aaron Cherian"""
 __email__ = "info@freemocap.org"
