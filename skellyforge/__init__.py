971ba45e2a6387d51e4801c23845f312af2e0066
philipqueen <queen.philip@gmail.com>
2024-08-28 12:01:17 -0700

    Bump version v2024.08.1007 -> v2024.08.1008

--- skellyforge/__init__.py
+++ skellyforge/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for basic_template_repo."""
 
 __package_name__ = "skellyforge"
-__version__ = "v2024.07.1006"
+__version__ = "v2024.08.1008"
 
 __author__ = """Aaron Cherian"""
 __email__ = "info@freemocap.org"
