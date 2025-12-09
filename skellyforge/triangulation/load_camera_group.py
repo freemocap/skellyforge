1411181308bfe739c0e6e6b07b913d7c2e06a998
philipqueen <queen.philip@gmail.com>
2025-12-09 16:36:56 -0700

    another typing error

--- skellyforge/triangulation/load_camera_group.py
+++ skellyforge/triangulation/load_camera_group.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 from typing import Union
 
-from skellyforge.calibration.freemocap_anipose import CameraGroup
+from skellyforge.calibration.freemocap_anipose import AniposeCameraGroup
 
 
 
 def load_camera_group_from_toml(
         camera_calibration_data_toml_path: Union[str, Path],
-) -> CameraGroup:
+) -> AniposeCameraGroup:
     try:
-        anipose_calibration_object = CameraGroup.load(str(camera_calibration_data_toml_path))
+        anipose_calibration_object = AniposeCameraGroup.load(str(camera_calibration_data_toml_path))
 
         return anipose_calibration_object
     except Exception as e:
